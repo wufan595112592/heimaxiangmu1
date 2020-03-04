7e1b299ba8289ec8e69ca51b56703495424dcc41
wufan595112592 <595112592@qq.com>
2020-03-04 11:08:45 +0800

    完成了401检验token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import ruter from '@/ruter'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 axios.interceptors.requst.use(function (config) {
   const token = localStorage.getItem('user-token')
@@ -8,4 +8,15 @@ axios.interceptors.requst.use(function (config) {
 }, function (error) {
   return Promise.reject(error)
 })
+
+axios.interceptors.response.use(function (response) {
+  return response.data ? response.data : {}
+}, function (error) {
+  if (error.response.status === 401) {
+    localStorage.removeItem('user-token')
+    ruter.push('/login')
+  }
+  return Promise.reject(error)
+})
+
 export default axios
